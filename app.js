a4e68d94e3d3ebe056c67413d612c4f5e0bfefd7
lucasmessias322 <68607934+lucasmessias322@users.noreply.github.com>
2021-10-18 14:26:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     //console.log("Acessou o Middleware!");
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://englishplusbrasil.herokuapp.com");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
